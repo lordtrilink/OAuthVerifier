5cee3fe1334a562f7cf6b344ce97061d4cf85abc
nigel <lordtrilink@gmail.com>
2014-11-30 08:39:53 -0500

    Adding Twitter oAuth support.

--- verifier.py
+++ verifier.py
@@ -1,8 +1,31 @@
+"""The MIT License
+
+Copyright (c) 2007 Nigel Brady
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. """
+
 __author__ = 'Nigel Brady'
 
 import urllib
 import urllib2
 import json
+import oauth
 
 
 class OAuthVerifier:
@@ -49,14 +72,59 @@ class OAuthException(Exception):
 
 
 class FacebookVerifier(OAuthVerifier):
-
     def __init__(self, token, user_id):
         OAuthVerifier.__init__(self, token, user_id, "https://graph.facebook.com/me")
 
 
 class GoogleVerifier(OAuthVerifier):
-
     def __init__(self, token, user_id):
         OAuthVerifier.__init__(self, token, user_id, "https://www.googleapis.com/oauth2/v1/tokeninfo", "user_id")
 
+
+class TwitterVerifier(OAuthVerifier):
+    consumer_key = None
+    consumer_secret = None
+    token_secret = None
+
+    def __init__(self, token, user_id, consumer_key, consumer_secret, token_secret):
+        OAuthVerifier.__init__(self, token, user_id,
+                               "https://api.twitter.com/1.1/account/verify_credentials.json",
+                               "id_str")
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.token_secret = token_secret
+
+    def verify(self):
+        consumer = oauth.OAuthConsumer(self.consumer_key, self.consumer_secret)
+        signature_method_hmac_sha1 = oauth.OAuthSignatureMethod_HMAC_SHA1()
+        oauth_token = oauth.OAuthToken(self.token, self.token_secret)
+
+        oauth_request = oauth.OAuthRequest.from_consumer_and_token(consumer,
+                                                                   token=oauth_token,
+                                                                   http_method='GET',
+                                                                   http_url=self.url)
+
+        oauth_request.sign_request(signature_method_hmac_sha1, consumer, oauth_token)
+
+        headers = oauth_request.to_header()
+
+        try:
+            req = urllib2.Request(self.url, headers=headers)
+            result = urllib2.urlopen(req)
+            response = result.read()
+
+            print response
+
+            result_dict = json.loads(response)
+
+            if self.user_id_field in result_dict and result_dict[self.user_id_field] == self.user_id:
+                return
+
+            raise OAuthException()
+
+        except urllib2.HTTPError as e:
+            if e.code == 401:
+                raise OAuthException()
+            else:
+                raise e
 
